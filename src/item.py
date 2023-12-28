937a1885bd549518c8530938fd1fd4553aead88f
Marat Zaripov <maratos96@gmail.com>
2023-12-28 22:07:15 +0300

    тест

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self._name = name  # Приватный атрибут name
         self.price = price
         self.quantity = quantity
         self.all.append(self)  # Добавляем созданный объект в список all
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if len(value) > 10:
+            self._name = value[:10]
+        else:
+            self._name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +45,32 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: str) -> None:
+        """
+        Инициализирует экземпляры класса Item данными из CSV-файла.
+
+        :param file_path: Путь к CSV-файлу с данными.
+        """
+        with open(file_path, 'r', newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(value: str) -> float:
+        """
+        Преобразует строку в число.
+
+        :param value: Строка, представляющая число.
+        :return: Преобразованное число.
+        """
+        return float(value)
+
+
+# Пример использования
+Item.instantiate_from_csv('src/items.csv')
